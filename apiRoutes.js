df2715f59b76f6f7b878b2a85b2700ce227c75ca
jsf5077 <jsf5077@gmail.com>
2019-07-20 00:30:08 -0400

    basic folder structure setup.

---
+++ apiRoutes.js
@@ -0,0 +1,26 @@
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);  
+  });
+  
+  app.post("/api/friends", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+  // Create New Characters - takes in JSON input
+app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    var newFriend = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+  
+    characters.push(newFriend);
+  
+    res.json(newFriend);
+  });
