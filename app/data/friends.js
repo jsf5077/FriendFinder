b62f1eece068a48a8aa796f0ac16b5b7202db59b
jsf5077 <jsf5077@gmail.com>
2019-07-23 02:08:39 -0400

    figured out how to properly post from survey.html to apiroutes. comparison logic complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,6 +7,22 @@ var friends = [{
     "name":"Jennifer",
     "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
     "scores":[ 3, 2, 4, 4, 5, 3, 2, 2, 4, 4]
+  }, {
+    "name":"Sammy",
+    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "scores":[ 3, 2, 5, 4, 5, 5, 2, 2, 4, 5]
+  }, {
+    "name":"Jeff",
+    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "scores":[ 1, 1, 4, 4, 2, 2, 3, 3, 5, 5]
+  }, {
+    "name":"Osiris",
+    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "scores":[ 3, 2, 4, 1, 5, 4, 1, 1, 1, 4]
+  }, {
+    "name":"Rhyse",
+    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "scores":[ 5, 5, 5, 5, 5, 3, 2, 2, 4, 4]
   }];
   
   module.exports = friends; 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,52 @@
-var path =require("path");
 // LOAD DATA
 var friends = require("../data/friends.js");
 
 // ROUTES
 module.exports = function(app) {
+  
   // Basic route that sends the user first to the AJAX Page
   app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
-  // app.post("/api/friends", function(req, res) {
-  //   console.log("TBD");
-  // });
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body
+    // console.log(newFriend);
+    var newScore = [];
+    for (i=0; i<newFriend.scores.length; i++) {
+      var newNum = newFriend.scores[i];
+      newScore[i] = parseInt(newNum);
+    }
+    newFriend.scores = newScore;
+    console.log(newScore);
+    console.log(friends[0].scores);
+
+    var match = {
+      name:"",
+      photo:"",
+      diff: 250
+    };
+
+    for (i = 0; i < friends.length; i++) {
+        var currentFriend = friends[i]
+        var totalDiff = 0;
+        // console.log(currentFriend)
+        console.log("checking "+currentFriend.name);
+
+      for (j = 0; j < currentFriend.scores.length; j++) {
+        totalDiff += Math.abs(parseInt(newScore[j]) - parseInt(currentFriend.scores[j]));
+      }
+      console.log("total difference with "+currentFriend.name+" is "+totalDiff);
+      if (totalDiff <= match.diff) {
+        match.name = currentFriend.name;
+        match.photo = currentFriend.photo;
+        match.diff = totalDiff;
+        console.log(currentFriend.name + " is the new current match");
+      }
+      console.log(match.diff + "\n");
+      
+    };
+    console.log(match);
+    friends.push(newFriend);
+    console.log(friends);
+  });
 };
