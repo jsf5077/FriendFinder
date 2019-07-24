686cc545974afaf14709b8b06f4a520f93108949
jsf5077 <jsf5077@gmail.com>
2019-07-23 23:00:12 -0400

    Modal is working and displaying the match results

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 // DATA
 var friends = [{
-    "name":"Nathan",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Rachel",
+    "photo":"https://pixel.nymag.com/imgs/daily/vulture/2014/12/17/17-rachel-green-jewish.o.jpg/a_610x408.w1200.h630.jpg",
     "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   }, {
-    "name":"Jennifer",
-    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "name":"Joey",
+    "photo":"https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest?cb=20180424154245",
     "scores":[ 3, 2, 4, 4, 5, 3, 2, 2, 4, 4]
   }, {
-    "name":"Sammy",
-    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "name":"Phoebe",
+    "photo":"https://cdn.playbuzz.com/cdn/0379afb3-f395-4816-8de0-9e1efd7cd0e7/96ec275b-3631-4242-8d5e-4cd804f90439.jpg",
     "scores":[ 3, 2, 5, 4, 5, 5, 2, 2, 4, 5]
   }, {
-    "name":"Jeff",
-    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "name":"Chandler",
+    "photo":"https://www.google.com/search?q=Chandler+Bing&sa=X&rlz=1C1CHBF_enUS776US776&stick=H4sIAAAAAAAAAONQFuLUz9U3MKzIMI5XAjON05IqTE8xIoSR2OZQtnFacropnI1QD2TnmcDZOUZFpxi5QGyTEtO8nFxkTtovRiG3oszUvJRiBeeMxKLE5JLUouJFrLxATl5KTmqRglNmXjoAOF5Gap8AAAA&biw=1280&bih=870&sxsrf=ACYBGNQOVstkz-kLcW51L7DBNXeBdDvYpQ:1563935103984&tbm=isch&source=iu&ictx=1&fir=VTE-vgeg5qaLiM%253A%252CQF1IdKwtjfko_M%252C%252Fm%252F03fbx5&vet=1&usg=AI4_-kRj_-snpao5bseM3sGbqSlaxfvlqw&ved=2ahUKEwiTi8yiwMzjAhUNT98KHZ9_DSEQ_B0wHXoECAkQAw#imgrc=V35zy8LVrp9J_M:&vet=1",
     "scores":[ 1, 1, 4, 4, 2, 2, 3, 3, 5, 5]
   }, {
-    "name":"Osiris",
-    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "name":"Monica",
+    "photo":"https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest?cb=20180426182234",
     "scores":[ 3, 2, 4, 1, 5, 4, 1, 1, 1, 4]
   }, {
-    "name":"Rhyse",
-    "photo":"https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
+    "name":"Ross",
+    "photo":"https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg/revision/latest?cb=20180424154547",
     "scores":[ 5, 5, 5, 5, 5, 3, 2, 2, 4, 4]
   }];
   
